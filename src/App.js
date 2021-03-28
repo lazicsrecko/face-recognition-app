c5cde0e7cdadd865b070fb2b7e875783e1fa95ee
nottebianca <sreckolazic94@gmail.com>
2021-03-28 23:01:18 +0200

    implemented face recognition box

--- src/App.js
+++ src/App.js
@@ -1,20 +1,58 @@
+import React, { useState } from 'react';
 import './App.css';
 import 'tachyons';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
+const clarifai = new Clarifai.App({
+  apiKey: '9e2b8beeaf164b688c3f6df9edfe8dba'
+})
 
 function App() {
+  const [userInput, setUserInput] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [box, setBox] = useState({});
+
+  const onInputChange = (event) => {
+    setUserInput(event.target.value);
+  }
+
+  const calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  const displayFaceBox = (box) => {
+    setBox(box);
+  }
+
+  const onSubmit = () => {
+    setImageUrl(userInput);
+    clarifai.models.predict(Clarifai.FACE_DETECT_MODEL, userInput)
+    .then(response => displayFaceBox(calculateFaceLocation(response)))
+    .catch(err => console.log(err))
+  }
   return (
     <div className="App">
-      <Particles className="particles" />
+      {/* <Particles className="particles" /> */}
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
+      <ImageLinkForm onInputChange={onInputChange} onButtonSubmit={onSubmit} />
+      <FaceRecognition box={box} imageUrl={imageUrl} />
     </div>
   );
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navigation = () => {
     return (
         <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-            <p className='p3 link dim black underline pa3 pointer'>Sign Out</p>
+            <p className='p3 link dim underline pa3 pointer'>Sign Out</p>
         </nav>
     )
 }
